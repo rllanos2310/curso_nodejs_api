a7e6bed7076ce27d03f39dbbb322901de4d43201
rogelio <rllanos2310@gmail.com>
2020-08-29 13:00:05 -0500

    [ADD] added pagination

--- src/routes/idea.routes.js
+++ src/routes/idea.routes.js
@@ -1,16 +1,17 @@
 const { Router } = require("express");
+const { AuthMiddleware, ParseIntMiddleware } = require("../middlewares")
 
 module.exports = function({ IdeaController }) {
     const router = Router();
 
-    router.get("", IdeaController.getAll);
-    router.get("/:ideaId", IdeaController.get);
-    router.get("/:userId/all", IdeaController.getUserIdeas);
-    router.post("", IdeaController.create);
-    router.patch("/:ideaId", IdeaController.update);
-    router.delete("/:ideaId", IdeaController.delete);
-    router.post("/:ideaId/upvote", IdeaController.upvoteIdea);
-    router.post("/:ideaId/downvote", IdeaController.downvoteIdea);
+    router.get("", [AuthMiddleware, ParseIntMiddleware], IdeaController.getAll);
+    router.get("/:ideaId", [AuthMiddleware], IdeaController.get);
+    router.get("/:userId/all", [AuthMiddleware], IdeaController.getUserIdeas);
+    router.post("", [AuthMiddleware], IdeaController.create);
+    router.patch("/:ideaId", [AuthMiddleware], IdeaController.update);
+    router.delete("/:ideaId", [AuthMiddleware], IdeaController.delete);
+    router.post("/:ideaId/upvote", [AuthMiddleware], IdeaController.upvoteIdea);
+    router.post("/:ideaId/downvote", [AuthMiddleware], IdeaController.downvoteIdea);
 
     return router;
 };
